7e5147a17448157f62f1f15587456dc135230a8d
Astric215 <davidiwd215@gmail.com>
2022-10-15 15:24:06 -0700

    enemy and player anims work

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemySpawner : MonoBehaviour
 {
@@ -20,6 +21,7 @@ public class EnemySpawner : MonoBehaviour
     {
         yield return new WaitForSeconds(interval);
         GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-6f, 6f), 0), Quaternion.identity);
+        newEnemy.GetComponent<Animator>().SetInteger("style", SceneManager.GetActiveScene().buildIndex);
         StartCoroutine(spawnEnemy(spawnInterval, enemy));
         if (spawnInterval > 0.5){
             spawnInterval -= 0.1f;
