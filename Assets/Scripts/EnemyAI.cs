3a988c9a0d802ac462c7ac6816e13a6c506b983e
Astric215 <davidiwd215@gmail.com>
2022-10-15 12:37:30 -0700

    simplifying enemy

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,31 +4,34 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    public float speed = 2f;
-    public bool shouldRotate = true;
+    [SerializeField]
+    private float speed = 2f;
+    [SerializeField]
+    private bool shouldRotate = true;
+    [SerializeField]
+    private LayerMask chasing;
+    [SerializeField]
+    private Vector3 dir;
 
-    public LayerMask chasing;
 
-    private Transform target;
+    private GameObject target;
     private Rigidbody2D rb;
     private Vector2 movement;
-    public Vector3 dir;
+    
 
     [SerializeField] int knockbackForce = 2;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        target = GameObject.FindWithTag("Player").transform;
+        target = GameObject.FindWithTag("Player");
 
     }
 
     private void Update()
     {
-        dir = target.position - transform.position;
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        dir.Normalize();
-        movement = dir;
+        //move towards the player
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, (float)0.003);
         if (shouldRotate)
         {
             transform.rotation = Quaternion.LookRotation(target.transform.position - transform.position);
@@ -36,24 +39,13 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
-    {
-        MoveCharacter(movement);
-    }
-
-    private void MoveCharacter(Vector2 dir)
-    {
-        rb.MovePosition((Vector2)transform.position + (dir * speed * Time.deltaTime));
-    }
-
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Player")
         {
-            GameObject player = GameObject.FindGameObjectsWithTag("Player")[0];
-            Rigidbody2D playerRb = player.GetComponent<Rigidbody2D>();
-            Vector3 knockback = (player.transform.position - transform.position).normalized * knockbackForce;
-            playerRb.AddForce(knockback, ForceMode2D.Impulse);
+            Rigidbody2D targetRb = target.GetComponent<Rigidbody2D>();
+            Vector2 knockback = (target.transform.position - transform.position).normalized * knockbackForce;
+            targetRb.AddForce(knockback, ForceMode2D.Impulse);
         }
     }
 }
